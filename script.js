e0bfc0d3a42f2671416ab3ffeddc764446b596db
RevanthRavi25 <revanthravi5@gmail.com>
2024-04-04 17:39:23 +0530

    update

--- script.js
+++ script.js
@@ -28,3 +28,24 @@ const changemaincon_full = () => {
     document.querySelector(".sidebar").classList.toggle("sidebar-close");
     document.querySelector("body").classList.toggle("fullcontent");
 };
+
+
+let passwd_view = true;
+let passwd_type = "password";
+let passwd_img = "/images/pwd_hide.png";
+
+function password_view() {
+  passwd_type = passwd_view ? "text" : "password";
+  passwd_img = passwd_view ? "/images/pwd_show.png" : "/images/pwd_hide.png";
+  passwd_view = !passwd_view;
+}
+function showPage(pageId) {
+    // Hide all pages
+    var pages = document.querySelectorAll('main > div');
+    pages.forEach(function(page) {
+        page.style.display = 'none';
+    });
+
+    // Show the selected page
+    document.getElementById(pageId).style.display = 'block';
+}
